23ef90acf258fb7ae035da3b64b1f953fff825fa
Hendar <hendarsundana@gmail.com>
2023-02-25 22:05:13 +0700

    + : add validation rooms null

--- controllers/dashbordController.js
+++ controllers/dashbordController.js
@@ -1,12 +1,17 @@
-const { completed } = require("../libs/room-status");
 const { User, Room } = require("./../models");
 
 module.exports = {
     index : async (req, res, next) => {
-
         // Count Data User
         const countUser = await User.count();
         const countRoom = await Room.count();
+        const countRoomCompleted = await Room.count(
+            {
+                where : {
+                    status: "COMPLETED"
+                }
+            }
+        );
 
         // buat section untuk query data Room dan join dengan data USER Winner
         const rooms = await Room.findAll({
@@ -22,7 +27,8 @@ module.exports = {
                 user: req.user,
                 content : [
                     { title : "USER", count: countUser, grid : 6  },
-                    { title : "ROOM", count: countRoom, grid: 6  }
+                    { title : "ROOM Finish", count: countRoomCompleted, grid: 6  },
+                    { title : "ROOM", count: countRoom, grid: 12  }
                   ],
                 rooms: rooms
             });
